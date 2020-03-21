251d2e19d66e3c92df8db19cb948acba8a281483
deb <lee025@gmail.com>
2020-03-20 20:31:00 -0400

    First Commit, User Auth, Pic CRUD

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,50 @@
+class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pics = Pic.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @pic = current_user.pics.build
+  end
+
+  def create
+    @pic = current_user.pics.build(pic_params)
+    if @pic.save
+      redirect_to @pic, notice: "Successfully posted pic!" 
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Pic was successfully updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path, notice: "Pic was successfully deleted"
+  end
+
+
+  private
+  def find_pic
+    @pic = Pic.find(params[:id])
+  end
+
+  def pic_params
+    params.require(:pic).permit(:title, :description, :image)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  
+  resources :pics
+  root 'pics#index'
+
+end
